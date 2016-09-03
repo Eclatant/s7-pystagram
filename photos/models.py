e236ac335b2bba54d2d79646acfc8659f6f29d05
Kay Cha <kay@hannal.net>
2016-09-03 17:57:15 +0900

    Tag 모델 추가

--- photos/models.py
+++ photos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     content = models.TextField('글 본문', max_length=500)
+    tags = models.ManyToManyField('Tag')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -12,4 +13,11 @@ class Comment(models.Model):
     content = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
