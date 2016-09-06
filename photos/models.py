7c56becf6617d25a6b5862e6a4a08a4a917d46c1
jaebul2006 <jaebul2006@naver.com>
2016-09-06 10:43:22 +0900

    피스타그램첫커밋

---
+++ photos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Post(models.Model):
+    content = models.TextField(max_length=500)
+    tags = models.ManyToManyField('Tag')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        title = self.content[0:50]
+        return '{}. {}'.format(self.pk, title)
+        #return '글 번호 : {}'.format(self.pk)
+
+    class Meta:
+        ordering = ('-created_at', '-pk',)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
