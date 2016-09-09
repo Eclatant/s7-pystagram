48d0abcf3d85583e3104a0e399bdb79d69c18df5
pcguard <pcguard@P0005-MBA13.local>
2016-09-09 19:39:39 +0900

    s7-pystagram homework

---
+++ pystagram2/photos/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Post(models.Model):
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "글번호:{}".format(self.pk)
+
+    class Meta:
+        ordering = ('-created_at', '-pk', )
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
