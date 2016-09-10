17771493a24f5a7c7101c571aa8ee5436e79ab0f
Kay Cha <kay@hannal.net>
2016-09-10 15:39:15 +0900

    pagination 직접 구현

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 
 def hello_world(request):
     return HttpResponse('hello world')
 
 
 def list_posts(request):
-    ctx = {}
+    per_page = 2
+    try:
+        page = request.GET.get('page', '')
+        if page.isdigit():
+            page = int(page)
+        if page < 1:
+            page = 1
+    except (TypeError, ValueError):
+        page = 1
+
+    posts = Post.objects.all().order_by('-created_at')
+
+    ctx = {
+        'posts': posts[(page-1)*per_page:page*per_page],
+    }
     return render(request, 'list.html', ctx)
 
