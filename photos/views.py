0c3445ff4d11951c975d4a6f5b2211be624f05a5
Kay Cha <kay@hannal.net>
2016-09-10 14:33:02 +0900

    list_posts() 뷰 함수와 url 연결

--- photos/views.py
+++ photos/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def hello_world(request):
     return HttpResponse('hello world')
 
+
+def list_posts(request):
+    ctx = {}
+    return render(request, 'list.html', ctx)
+
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from photos.views import hello_world
+from photos import views
 
 
 urlpatterns = [
-    url(r'^hello/$', hello_world),
+    url(r'^$', views.list_posts),
+    url(r'^hello/$', views.hello_world),
     url(r'^admin/', admin.site.urls),
 ]
 
