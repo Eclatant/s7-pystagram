cb35c07c063d9cecb7a8985b3c8e9620e93bac5a
Kay Cha <kay@hannal.net>
2016-09-24 17:02:58 +0900

    login.html 추가

--- pystagram/urls.py
+++ pystagram/urls.py
@@ -9,6 +9,7 @@ from photos import views
 urlpatterns = [
     url(r'^photos/', include('photos.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', login, name='login_url'),
+    url(r'^login/$', login,
+        {'template_name': 'login.html'}, name='login_url'),
 ]
 
