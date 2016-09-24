a9d471f9ed57aa7bde2c3fa200d76a8e79efa9f7
Kay Cha <kay@hannal.net>
2016-09-24 15:22:53 +0900

    validation 순서에 예외 상황 추가

--- photos/forms.py
+++ photos/forms.py
@@ -24,8 +24,8 @@ class PostForm(forms.ModelForm):
         return content  # 주의! 반드시 정제된 결과를 return 함.
 
     def clean(self):
-        content = self.cleaned_data['content']
-        if '띨띨이' in content:
+        content = self.cleaned_data.get('content')
+        if content and '띨띨이' in content:
             _msg = '방심하지 마라! 금지어 있다: {}'.format('띨띨이')
             self.add_error('content', _msg)
 
