2ecbc4d374277ba5d8691e40e3ab70540f0cd4df
Kay Cha <kay@hannal.net>
2016-09-24 15:12:59 +0900

    본문에 금지어 validation 추가

--- photos/forms.py
+++ photos/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ValidationError
 
 from .models import Post
 
@@ -13,4 +14,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['content', 'tags']
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if '바보' in content:
+            _msg = '본문에 금지어가 있습니다: {}'.format('바보')
+            raise ValidationError(_msg)
+
+        return content  # 주의! 반드시 정제된 결과를 return 함.
+
+
+
+
 
