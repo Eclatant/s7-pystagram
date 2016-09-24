b2b6557ac37b15122b0bac4280e806db716a33fc
Kay Cha <kay@hannal.net>
2016-09-24 14:56:10 +0900

    Post 모델 폼 추가

--- photos/forms.py
+++ photos/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 
+from .models import Post
+
 
 class PostSimpleForm(forms.Form):
     content = forms.CharField(widget=forms.Textarea)
 
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['content', 'tags']
 
